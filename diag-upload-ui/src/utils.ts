df4637122ae68f654dc68e9d324f0c5fb0fe0f36
TomMcAvoy <elmstreet2011@gmail.com>
2024-10-17 02:21:11 -0300

    metadata

--- diag-upload-ui/src/utils.ts
+++ diag-upload-ui/src/utils.ts
@@ -1,3 +1,7 @@
+import { message, Button, Descriptions, DescriptionsItem } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
 export type FileType = {
   id: string;
   fileName: string;
@@ -69,3 +73,50 @@ export const deleteFile = async (fileId: string): Promise<{ message: string }> =
   }
   throw new Error(await response.text());
 };
+
+const File = ({ appState }: { appState: string }) => {
+  const { id } = useParams();
+  const [file, setFile] = useState<FileType>();
+  const navigate = useNavigate();
+
+  const getFile = useCallback(async () => {
+    if (!id) return;
+    try {
+      const fileMeta = await getFileMeta(id);
+      setFile(fileMeta);
+    } catch (error) {
+      message.error('File not found');
+    }
+  }, [id]);
+
+  const handleDelete = async () => {
+    if (!file) return;
+    try {
+      await deleteFile(file.id);
+      navigate('/');
+    } catch (error) {
+      message.error('Cannot delete file');
+    }
+  };
+
+  useEffect(() => {
+    getFile();
+  }, [getFile, appState]);
+
+  if (!file) return null;
+  return (
+    <Descriptions title="File Data" column={1} bordered>
+      <Descriptions.Item label="ID">{file.id}</Descriptions.Item>
+      <Descriptions.Item label="File Name">{file.fileName}</Descriptions.Item>
+      <Descriptions.Item label="Checksum">{file.checksum}</Descriptions.Item>
+      <Descriptions.Item label="Creation Date">
+        {new Date(file.creationDate).toLocaleString()}
+      </Descriptions.Item>
+      <Descriptions.Item label="Danger" labelStyle={{ color: 'red' }}>
+        <Button onClick={handleDelete}>Delete File</Button>
+      </Descriptions.Item>
+    </Descriptions>
+  );
+};
+
+export default File;
